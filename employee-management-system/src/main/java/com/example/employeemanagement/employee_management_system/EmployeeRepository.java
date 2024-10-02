31b86f5ba493bc4f1820e047d3b3ce7ab406d343
siddhisinghrathor <156497221+siddhisinghrathor@users.noreply.github.com>
2024-10-02 16:54:09 +0530

    Delete employee-management-system directory

--- employee-management-system/src/main/java/com/example/employeemanagement/employee_management_system/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeemanagement.employee_management_system;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
-
